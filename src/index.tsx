11bf75c0a29cd3360569bdc17e712fde357f7fb9
Norman <normanrmico@gmail.com>
2024-07-14 23:47:36 +0200

    task1 - KAN-10 - fix a small code formatting issue on index.tsx for null escape and consistent quotations

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import {createRoot, Root} from 'react-dom/client'
-import App from './components/App'
+import React from "react"
+import {createRoot, Root} from "react-dom/client"
+import App from "./components/App"
 
-const container  = document.getElementById("root") as HTMLElement;
+const container:HTMLElement  = document.getElementById("root")!;
 const root:Root = createRoot(container);
 
 root.render(<App/>);
