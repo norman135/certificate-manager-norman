b6bd5f9e975008d1f743debb09983c08f2faf2fc
Norman <normanrmico@gmail.com>
2024-07-17 11:53:16 +0200

    task1 - KAN-2 - add error handling on root

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from "react"
 import {createRoot, Root} from "react-dom/client"
-import App from "./components/App"
+import App from "./App"
 
 const container:HTMLElement  = document.getElementById("root")!;
-const root:Root = createRoot(container);
 
-root.render(<App/>);
+if (container) {
+    const root:Root = createRoot(container);
+
+    root.render(<App/>);
+} else {
+    console.log("Root element does not exist.");
+};
