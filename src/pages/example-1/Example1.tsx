70309863efe9a47109a5eb0d4349a2f0f92a7be1
Norman <normanrmico@gmail.com>
2024-07-19 17:02:53 +0200

    task2 - KAN-11 - formatting and adding table

--- src/pages/example-1/Example1.tsx
+++ src/pages/example-1/Example1.tsx
@@ -1,7 +1,72 @@
 import { FC } from 'react';
+import './Example1.css';
+import Button from '../../components/button/Button';
+
+type Certificate = {
+	supplier: string;
+	type: string;
+	validFrom: Date;
+	validTo: Date;
+};
 
 const Example1: FC = (): JSX.Element => {
-	return <h1>Example 1</h1>;
+	const certificates: Certificate[] = [
+		{
+			supplier: 'DAIMLER AG, 1, Berlin',
+			type: 'Permission of Printing',
+			validFrom: new Date('08-21-2017'),
+			validTo: new Date('08-26-2017'),
+		},
+		{
+			supplier: 'ANDEMIS GmBH, 1, Stuttgart',
+			type: 'OHSAS 18001',
+			validFrom: new Date('08-18-2017'),
+			validTo: new Date('08-24-2017'),
+		},
+		{
+			supplier: 'ANDEMIS GmBH, 1, Stuttgart',
+			type: 'Permission of Printing',
+			validFrom: new Date('10-04-2017'),
+			validTo: new Date('10-10-2017'),
+		},
+	];
+
+	const formatDate = (date: Date): string => {
+		const date_ = date.getDate();
+		const month = date.getMonth();
+		const year = date.getFullYear();
+
+		return `${date_}.${month}.${year}`;
+	};
+
+	return (
+		<>
+			<div className="certificates-table">
+				<table>
+					<thead>
+						<tr>
+							<th></th>
+							<th>Supplier</th>
+							<th>Certificate type</th>
+							<th>Valid from</th>
+							<th>Valid to</th>
+						</tr>
+					</thead>
+					<tbody>
+						{certificates.map((certificate, index) => (
+							<tr>
+								<td></td>
+								<td>{certificate.supplier}</td>
+								<td>{certificate.type}</td>
+								<td>{formatDate(certificate.validFrom)}</td>
+								<td>{formatDate(certificate.validTo)}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
 };
 
 export default Example1;
