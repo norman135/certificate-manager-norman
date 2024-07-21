6f63aa9bf09fd56896a75a2556eda0ceca855e8d
Norman <normanrmico@gmail.com>
2024-07-21 09:51:45 +0200

    task2 - KAN-11 - sub-item-menu bug fixes for unclickable area

--- src/pages/example-1/Example1.tsx
+++ src/pages/example-1/Example1.tsx
@@ -8,36 +8,36 @@ type Certificate = {
 	validTo: Date;
 };
 
+const certificates: Certificate[] = [
+	{
+		supplier: 'DAIMLER AG, 1, Berlin',
+		type: 'Permission of Printing',
+		validFrom: new Date('08-21-2017'),
+		validTo: new Date('08-26-2017'),
+	},
+	{
+		supplier: 'ANDEMIS GmBH, 1, Stuttgart',
+		type: 'OHSAS 18001',
+		validFrom: new Date('08-18-2017'),
+		validTo: new Date('08-24-2017'),
+	},
+	{
+		supplier: 'ANDEMIS GmBH, 1, Stuttgart',
+		type: 'Permission of Printing',
+		validFrom: new Date('10-04-2017'),
+		validTo: new Date('10-10-2017'),
+	},
+];
+
+const formatDate = (date: Date): string => {
+	const date_ = date.getDate();
+	const month = date.getMonth();
+	const year = date.getFullYear();
+
+	return `${date_}.${month}.${year}`;
+};
+
 const Example1: FC = (): JSX.Element => {
-	const certificates: Certificate[] = [
-		{
-			supplier: 'DAIMLER AG, 1, Berlin',
-			type: 'Permission of Printing',
-			validFrom: new Date('08-21-2017'),
-			validTo: new Date('08-26-2017'),
-		},
-		{
-			supplier: 'ANDEMIS GmBH, 1, Stuttgart',
-			type: 'OHSAS 18001',
-			validFrom: new Date('08-18-2017'),
-			validTo: new Date('08-24-2017'),
-		},
-		{
-			supplier: 'ANDEMIS GmBH, 1, Stuttgart',
-			type: 'Permission of Printing',
-			validFrom: new Date('10-04-2017'),
-			validTo: new Date('10-10-2017'),
-		},
-	];
-
-	const formatDate = (date: Date): string => {
-		const date_ = date.getDate();
-		const month = date.getMonth();
-		const year = date.getFullYear();
-
-		return `${date_}.${month}.${year}`;
-	};
-
 	return (
 		<>
 			<div className="certificates-table">
