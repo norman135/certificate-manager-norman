64cbc8ee9f66de59dcbd61c1af9a8a722e9f39bc
Norman <normanrmico@gmail.com>
2024-07-22 11:18:57 +0200

    task4 - KAN-32 - add dropdown

--- src/pages/example-1/Example1.tsx
+++ src/pages/example-1/Example1.tsx
@@ -7,6 +7,28 @@ import formatDate from '../../common/utils/format-date.utils';
 import './Example1.css';
 
 const Example1: FC = (): JSX.Element => {
+	const [change, setChange] = useState<number>(0);
+
+	const deleteCertificate = (index: number) => {
+		certificates.splice(index);
+		setChange(change + 1);
+	}
+
+	const certificateDropDown = (id: number, index: number): DropDownItem[] => {
+		const navigate = useNavigate();
+	
+		return [
+			{
+				name: "Edit",
+				action: () => {navigate(`/edit-certificate/${id}`)}
+			},
+			{
+				name: "Delete",
+				action: () => {deleteCertificate(index)}
+			}
+		]
+	}
+
 	return (
 		<>
 			<Button
