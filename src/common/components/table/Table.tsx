3e3d6fc3f112b61fc9c741d8b10bfc7b8b229f26
Norman <normanrmico@gmail.com>
2024-07-25 10:20:06 +0200

    task6 - KAN-48 - update formatting

--- src/common/components/table/Table.tsx
+++ src/common/components/table/Table.tsx
@@ -1,33 +1,32 @@
 import { ReactNode } from 'react';
 import './Table.css';
 
-
 interface TableProps<T> {
-  columns: string[];
-  data: T[][];
+	columns: string[];
+	data: T[][];
 }
 
-const Table = <T extends {}> ({ columns, data }: TableProps<T>): JSX.Element => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((column) => (
-            <th key={column}>{column}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex.toString()}>
-            {row.map((column, colIndex) => (
-              <td key={colIndex.toString()}>{column as ReactNode}</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+const Table = <T extends {}>({ columns, data }: TableProps<T>): JSX.Element => {
+	return (
+		<table>
+			<thead>
+				<tr>
+					{columns.map((column) => (
+						<th key={column}>{column}</th>
+					))}
+				</tr>
+			</thead>
+			<tbody>
+				{data.map((row, rowIndex) => (
+					<tr key={rowIndex.toString()}>
+						{row.map((column, colIndex) => (
+							<td key={colIndex.toString()}>{column as ReactNode}</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 };
 
 export default Table;
