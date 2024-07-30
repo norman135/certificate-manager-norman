28178e98ada172f3589f9f4f5336e0e80c26bd09
Norman <normanrmico@gmail.com>
2024-07-30 11:17:50 +0200

    task6 - KAN-48 - push requested changes

--- src/common/components/supplier-user-lookup/SupplierLookup.tsx
+++ src/common/components/supplier-user-lookup/SupplierLookup.tsx
@@ -22,13 +22,7 @@ const SupplierLookup: FC<SupplierLookupProps> = ({
 	const [selectedSupplier, setSelectedSupplier] =
 		useState<Supplier>(initialSupplier);
 
-	useEffect(() => {
-		getAllSuppliers().then((data) => {
-			setSuppliersBuffer(data);
-		});
-	}, []);
-
-	const SupplierSearch = async (): Promise<void> => {
+	const supplierSearch = async (): Promise<void> => {
 		const result = await searchSuppliers({
 			name: supplierInfo.name,
 			index: supplierInfo.indexValue,
@@ -101,7 +95,7 @@ const SupplierLookup: FC<SupplierLookupProps> = ({
 						bg="#265b7a"
 						type="button"
 						to=""
-						onClick={SupplierSearch}
+						onClick={supplierSearch}
 					/>
 					<Button
 						name="Reset"
