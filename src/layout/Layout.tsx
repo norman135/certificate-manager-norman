07910bc6919e15e31d4b065396a835f4484e5c8f
Norman <normanrmico@gmail.com>
2024-07-31 09:55:07 +0200

    task9 - KAN-89 - add user dropdown

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC } from 'react';
+import { ChangeEvent, FC, useEffect, useState } from 'react';
 import SideBar from './side-bar/SideBar';
 import './Layout.css';
 import Select from '../common/components/select/Select';
@@ -7,15 +7,53 @@ import {
 	toSelectedLocale,
 	useLanguageContext,
 } from '../common/language/Language';
+import { initialUser } from '../common/utils/user.utils';
+import User from '../common/models/user.model';
+import getAllUsers from '../common/db/user-service';
+import { useCurrentUserContext } from '../common/user/User';
 
 interface LayoutProps {
 	children: JSX.Element;
 }
 const Layout: FC<LayoutProps> = ({ children }): JSX.Element => {
+	const [users, setUsers] = useState<User[]>([initialUser]);
 	const { language, setLanguage } = useLanguageContext();
+	const { user, setUser } = useCurrentUserContext();
 
 	const handleLanguageChange = (e: ChangeEvent<HTMLSelectElement>) => {
 		setLanguage(e.target.value);
+	};
+
+	useEffect(() => {
+		const getUsers = async () => {
+			const allUsers = await getAllUsers();
+
+			setUsers(allUsers);
+		};
+
+		getUsers;
+	}, []);
+
+	const getUserOptions = () => {
+		const options = [
+			{
+				value: initialUser.id,
+				text: 'Choose User',
+			},
+		];
+
+		users.forEach((user) => {
+			options.push({
+				value: user.id,
+				text: user.firstName,
+			});
+		});
+
+		return options;
+	};
+
+	const handleUserChange = (e: ChangeEvent<HTMLSelectElement>) => {
+		setUser(users.filter((user) => user.id === e.target.value)[0]);
 	};
 
 	return (
@@ -24,6 +62,16 @@ const Layout: FC<LayoutProps> = ({ children }): JSX.Element => {
 			<main className="main-content">
 				<div className="main-top-bar">
 					<div className="main-top-bar-options">
+						<div className="main-top-bar-option">
+							<div className="main-top-bar-option-label">
+								{toSelectedLocale('user', language)}
+							</div>
+							<Select
+								options={getUserOptions()}
+								value={user.id}
+								onChange={handleUserChange}
+							/>
+						</div>
 						<div className="main-top-bar-option">
 							<div className="main-top-bar-option-label">
 								{toSelectedLocale('language', language)}
