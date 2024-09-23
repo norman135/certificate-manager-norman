df0c0e5d57661b08a4e3ea79ce4e4d0fbcd0af52
Norman <normanrmico@gmail.com>
2024-09-23 16:06:37 +0200

    Simple scaffold script text file

--- backend/CertificatesManagerApi/Tools/TextFile.bat
+++
@@ -1,6 +0,0 @@
-﻿@echo off
-echo Scaffolding the DbContext and entities...
-
-Scaffold-DbContext "Host=localhost;Database=certificatemanager;Username=postgres;Password=password" Npgsql.EntityFrameworkCore.PostgreSQL -OutputDir Entities -ContextDir Contexts -Context CertificateManagerContext
-
-echo Scaffold complete!
