d54877b32b5f655d9ec7213770574f655b099b0f
Norman <normanrmico@gmail.com>
2024-09-24 11:49:58 +0200

    updating scaffold tool

--- backend/CertificatesManagerApi/Tools/Scaffold.ps1
+++ backend/CertificatesManagerApi/Tools/Scaffold.ps1
@@ -1,7 +1,12 @@
-﻿$connectionString = "Host=localhost;Database=certificatemanager;Username=postgres;Password=password"
+﻿$appSettingsFile = "appsettings.json"
+
+$appSettings = Get-Content $appSettingsFile -Raw | ConvertFrom-Json
+
+$connectionString = $appSettings.ConnectionStrings.DefaultConnection
+
 $provider = "Npgsql.EntityFrameworkCore.PostgreSQL"
 $entitiesDir = "Entities"
 $contextDir = "Contexts"
 $contextName = "CertificateManagerContext"
 
-dotnet-ef dbcontext scaffold $connectionString  -output-dir $entitiesDir -context-dir $contextDir  -context $contextName
+dotnet-ef dbcontext scaffold $connectionString  --output-dir $entitiesDir --context-dir $contextDir  --context $contextName
